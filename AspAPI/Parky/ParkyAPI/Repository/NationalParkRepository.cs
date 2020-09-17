5fe453672acff3ba38177ccf5b27453d34b76290
Eugene Dianov <Eugenedia@gmail.com>
2020-09-17 13:08:01 +0300

    Add Repository and DTOs

---
+++ AspAPI/Parky/ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,64 @@
+﻿using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        public NationalPark GetNationalPark(int nationalParkId)
+        {
+            return db.NationalParks.FirstOrDefault(a => a.Id == nationalParkId);   
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            return db.NationalParks.OrderBy(a => a.Name).ToList();
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            bool value = db.NationalParks.Any(a => a.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return db.NationalParks.Any(a => a.Id == id);
+        }
+
+        public bool Save()
+        {
+            return db.SaveChanges() >= 0;
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            db.NationalParks.Update(nationalPark);
+            return Save();
+        }
+    }
+}
